91a967753e7b663684cdd8b55130ba31f98c2aad
Dereck <b00003398@student.itb.ie>
2015-12-03 18:37:59 +0000

    Added login/register and other files

---
+++ Documents/UltimateRunner-master/UltimateRunner/Assets/KelvinPlayGround/Script/GameLength.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLength {
+
+
+	private static  int name;
+	public int Name{
+
+		get
+		{
+			return name;
+
+		}
+		set
+		{
+
+			name = value;
+
+		}
+
+
+
+	}
+
+
+
+}
---
+++ Documents/UltimateRunner-master/UltimateRunner/Assets/StartMenu/StrartMenuScript/MenuScript.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+
+    /**
+        public global variables for the quit menu canvas.
+    */
+
+    public Canvas quitMenu, gamemode, gamelength;
+    public Button startText;
+    public Button exitText;
+	public Button singlePlayer , multiplayer, quickgame , longgame;
+
+	GameLength gl = new GameLength();
+
+
+	// Use this for initialization
+	void Start () {
+
+        // get all UI component inside the (quitMenu canvas)
+        quitMenu = quitMenu.GetComponent<Canvas>();
+		gamemode = gamemode.GetComponent<Canvas>();
+		gamelength = gamelength.GetComponent<Canvas>();
+        quitMenu.enabled = false;
+		gamemode.enabled = false;
+		gamelength.enabled = false;
+
+        startText = startText.GetComponent<Button>();
+        exitText = startText.GetComponent<Button>();
+
+		singlePlayer = startText.GetComponent<Button>();
+		multiplayer = startText.GetComponent<Button>();
+
+
+		quickgame = startText.GetComponent<Button>();
+		longgame = startText.GetComponent<Button>();
+
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public void ExitPress() {
+
+        quitMenu.enabled = true;
+        startText.enabled = false;
+        exitText.enabled = false;
+
+    }
+
+
+    public void NoPress(){
+
+        quitMenu.enabled = false;
+        startText.enabled = true;
+        exitText.enabled = true;
+
+
+
+
+    }
+
+	public void PlayPress(){
+		gamemode.enabled = true;
+		startText.enabled = false;
+		exitText.enabled = false;
+
+
+	}
+
+	public void SinglePlayerPress(){
+		gamemode.enabled = false;
+		startText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void gameselected(){
+
+		gamemode.enabled = false;
+		gamelength.enabled = true;
+
+
+	}
+
+	public void quickselected(){
+
+		gl.Name = 3;
+		Debug.Log (gl.Name);
+		Application.LoadLevel(1);
+
+
+	}
+
+	public void hardselected(){
+		
+		gl.Name = 9;
+		Debug.Log (gl.Name);
+		Application.LoadLevel(1);
+		
+		
+	}
+
+   
+
+	public void StartGame() {
+
+        Application.LoadLevel(1);
+    }
+
+
+    public void ExitGame() {
+
+        Application.Quit();
+
+    }
+
+
+
+
+}
